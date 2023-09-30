ba7fbf63c73fa1d32be53cd81004e5c97c76f846
Anugautam1998 <anu.gautam608@gmail.com>
2023-09-30 19:12:19 +0530

    Anu |jbdl-58 Maven and Stream assignment

--- ANU_MAVEN_JAR_DEPENDENCY_PROJECT_JDBL_58'/mysql/src/main/java/com/example/sql/Main.java
+++ ANU_MAVEN_JAR_DEPENDENCY_PROJECT_JDBL_58'/mysql/src/main/java/com/example/sql/Main.java
@@ -1,9 +1,7 @@
 package com.example.sql;
 
-import org.springframework.boot.SpringApplication;
 
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
     }
 }
